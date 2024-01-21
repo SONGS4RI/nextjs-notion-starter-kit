e37a74f76486d8bc0c9bf21725c52daa8c88b842
이자훈 <jahlee@student.42seoul.kr>
2024-01-21 21:55:50 +0900

    :sparkles: feat: hits 추가

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -80,6 +80,10 @@ export const NotionPageHeader: React.FC<{
 
           <ToggleThemeButton />
 
+          <div>
+            <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fsongs4ri.vercel.app&count_bg=%23CDCDCD&title_bg=%23DEB887&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/>
+          </div>
+
           {isSearchEnabled && <Search block={block} title={null} />}
         </div>
       </div>
